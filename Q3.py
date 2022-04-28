1a26a227a04e6345cd48329617bb63a64f4e342c
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-04-27 18:18:53 -0700

    finsihed

---
+++ Q3.py
@@ -0,0 +1,3 @@
+def rev_string (my_str):
+    return my_str[::-1]
+print(rev_string("swag"))
---
+++ Q4.py
@@ -0,0 +1,4 @@
+def num_within(x,a,b):
+  return x in range(a,b+1)
+     
+print(num_within(3,2,4))     
